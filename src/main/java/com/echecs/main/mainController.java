a3c0cabf4bc93200aea8a43214a35dc4f25b2f47
Théo BARCELO <theo.barcelo@etu.univ-amu.fr>
2024-06-07 11:19:39 +0200

    merge + tournoi

--- src/main/java/com/echecs/main/mainController.java
+++ src/main/java/com/echecs/main/mainController.java
@@ -74,9 +74,12 @@ public class mainController {
     private Double height = 0.0;
     private Double width = 0.0;
 
+<<<<<<< Updated upstream
     private List<String> joueurs;
+=======
     private List<Joueur> joueursListe = new ArrayList<>();
     private int joueursSize = 2;
+>>>>>>> Stashed changes
 
     private List<Joueur> joueursPartie = new ArrayList<>();
 
@@ -181,6 +184,10 @@ public class mainController {
         String selectedTemps = tempsComboBox.getValue();
         tempsInitialBlancs = Integer.parseInt(selectedTemps) * 60;
         tempsInitialNoirs = Integer.parseInt(selectedTemps) * 60;
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
         reinitialiserPlateau();
         configurerPieces();
         mettreAJourPieces();
